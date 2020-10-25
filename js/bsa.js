7570366fc8dec85c5df4e5317a2f9c9b2690c5bd
unknown <you@example.com>
2020-10-25 11:41:45 +0600

    initial commit

---
+++ js/bsa.js
@@ -0,0 +1,18 @@
+
+
+document.write('<link href="/assets/css/bsa2.css" rel="stylesheet" type="text/css" media="screen" />');
+document.write('<script async type="text/javascript" src="http://cdn.adpacks.com/adpacks.js?zoneid=1386&serve=C6SI42Y&placement=wwwquenesscom" id="_adpacks_js"></script>');
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-7781887-1']);
+_gaq.push(['_trackPageview']);
+
+
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
